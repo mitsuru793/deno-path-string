145f373d79a18ca0b321082b24e5cdc813b923d9
mitsuru793 <mitsuru793@gmail.com>
2024-11-02 01:19:46 +0900

    feat: add function doesFileExist

---
+++ path-string/src/exist/does_file_exist_test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "@std/assert";
+import { doesFileExist } from "./does_file_exist.ts";
+
+Deno.test("doesFileExist", () => {
+  const dir = Deno.makeTempDirSync();
+  const file = Deno.makeTempFileSync();
+
+  const testcases: [string, boolean][] = [
+    [dir, false],
+    [file, true],
+    ["missing", false],
+  ];
+  testcases.forEach(([target, expected]) => {
+    const got = doesFileExist(target);
+    assertEquals(got, expected);
+  });
+});
