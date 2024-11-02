72e7a10b21a58febfbb7d992c60217968345ba72
mitsuru793 <mitsuru793@gmail.com>
2024-11-02 11:35:26 +0900

    style: run deno fmt

--- path-string/src/exist/mod.ts
+++ path-string/src/exist/mod.ts
@@ -1,3 +1,3 @@
-export * from './does_path_exist.ts'
-export * from './does_dir_exist.ts'
-export * from './does_file_exist.ts'
+export * from "./does_path_exist.ts";
+export * from "./does_dir_exist.ts";
+export * from "./does_file_exist.ts";
