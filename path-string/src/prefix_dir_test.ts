d35c9c920674a1d74d2a4a1bf11fd6b82917c66d
mitsuru793 <mitsuru793@gmail.com>
2024-11-02 12:36:03 +0900

    refactor(test): prefix_dir_test.ts with tableDrivenTest() and tableDrivenTestThrow()

--- path-string/src/prefix_dir_test.ts
+++ path-string/src/prefix_dir_test.ts
@@ -7,117 +7,144 @@ type TestCase = {
   expected: ReturnType<typeof prefixDir>;
 };
 
+function tableDrivenTest(
+  testname: string,
+  testcases: TestCase[],
+) {
+  Deno.test(testname, async (t) => {
+    for (const testcase of testcases) {
+      await t.step(testcase.label, () => {
+        const got = prefixDir(...testcase.inputs);
+        assertEquals(got, testcase.expected);
+      });
+    }
+  });
+}
+
 type ThrowTestCase = {
   label: string;
   inputs: Parameters<typeof prefixDir>;
 };
 
-Deno.test("prefixDir - argument paths", async (t) => {
-  const tests: TestCase[] = [
-    {
-      label: "if it is empty",
-      inputs: ["dir/", []],
-      expected: [],
-    },
-    {
-      label: "if it has file",
-      inputs: ["dir/", ["f1"]],
-      expected: ["dir/f1"],
-    },
-    {
-      label: "if it has dir",
-      inputs: ["dir/", ["subDir/"]],
-      expected: ["dir/subDir/"],
-    },
-    {
-      label: "if it has nested path",
-      inputs: ["dir/", ["subDir/f1"]],
-      expected: ["dir/subDir/f1"],
-    },
-    {
-      label: "if it has some elements",
-      inputs: ["dir/", ["f1", "subDir/"]],
-      expected: ["dir/f1", "dir/subDir/"],
-    },
-  ];
+function tableDrivenTestThrow(
+  testname: string,
+  testcases: ThrowTestCase[],
+) {
+  Deno.test(testname, async (t) => {
+    for (const testcase of testcases) {
+      await t.step(testcase.label, () => {
+        assertThrows(() => {
+          prefixDir(...testcase.inputs);
+        });
+      });
+    }
+  });
+}
 
-  for (const test of tests) {
-    await t.step(test.label, () => {
-      const got = prefixDir(...test.inputs);
-      assertEquals(got, test.expected);
-    });
-  }
-});
+tableDrivenTest("prefixDir - argument paths", [
+  {
+    label: "if it is empty",
+    inputs: ["dir/", []],
+    expected: [],
+  },
+  {
+    label: "if it has file",
+    inputs: ["dir/", ["f1"]],
+    expected: ["dir/f1"],
+  },
+  {
+    label: "if it has dir",
+    inputs: ["dir/", ["subDir/"]],
+    expected: ["dir/subDir/"],
+  },
+  {
+    label: "if it has nested path",
+    inputs: ["dir/", ["subDir/f1"]],
+    expected: ["dir/subDir/f1"],
+  },
+  {
+    label: "if it has some elements",
+    inputs: ["dir/", ["f1", "subDir/"]],
+    expected: ["dir/f1", "dir/subDir/"],
+  },
+]);
 
-Deno.test("prefixDir - arugment prefix", async (t) => {
-  const tests: TestCase[] = [
-    {
-      label: "if it has suffix with slash",
-      inputs: ["dir/", ["f1"]],
-      expected: ["dir/f1"],
-    },
-    {
-      label: "if it has prefix and suffix with slash",
-      inputs: ["/dir/", ["f1"]],
-      expected: ["/dir/f1"],
-    },
-    {
-      label: "if it has prefix and suffix with slash",
-      inputs: ["/dir/", ["f1"]],
-      expected: ["/dir/f1"],
-    },
-    {
-      label: "if it is only slash",
-      inputs: ["/", ["f1"]],
-      expected: ["/f1"],
-    },
-  ];
-  for (const test of tests) {
-    await t.step(test.label, () => {
-      const got = prefixDir(...test.inputs);
-      assertEquals(got, test.expected);
-    });
-  }
+tableDrivenTest("prefixDir - argument paths", [
+  {
+    label: "if it is empty",
+    inputs: ["dir/", []],
+    expected: [],
+  },
+  {
+    label: "if it has file",
+    inputs: ["dir/", ["f1"]],
+    expected: ["dir/f1"],
+  },
+  {
+    label: "if it has dir",
+    inputs: ["dir/", ["subDir/"]],
+    expected: ["dir/subDir/"],
+  },
+  {
+    label: "if it has nested path",
+    inputs: ["dir/", ["subDir/f1"]],
+    expected: ["dir/subDir/f1"],
+  },
+  {
+    label: "if it has some elements",
+    inputs: ["dir/", ["f1", "subDir/"]],
+    expected: ["dir/f1", "dir/subDir/"],
+  },
+]);
 
-  const throwTests: ThrowTestCase[] = [
-    {
-      label: "if it has no suffix with slash",
-      inputs: ["base", ["f1"]],
-    },
-    {
-      label: "if it is empty",
-      inputs: ["", ["f1"]],
-    },
-  ];
-  for (const test of throwTests) {
-    await t.step(test.label, () => {
-      assertThrows(() => prefixDir(...test.inputs));
-    });
-  }
-});
+tableDrivenTest("prefixDir - arugment prefix for valid", [
+  {
+    label: "if it has suffix with slash",
+    inputs: ["dir/", ["f1"]],
+    expected: ["dir/f1"],
+  },
+  {
+    label: "if it has prefix and suffix with slash",
+    inputs: ["/dir/", ["f1"]],
+    expected: ["/dir/f1"],
+  },
+  {
+    label: "if it has prefix and suffix with slash",
+    inputs: ["/dir/", ["f1"]],
+    expected: ["/dir/f1"],
+  },
+  {
+    label: "if it is only slash",
+    inputs: ["/", ["f1"]],
+    expected: ["/f1"],
+  },
+]);
 
-Deno.test("prefixDir - combine continuous slash into one", async (t) => {
-  const tests: TestCase[] = [
-    {
-      label: "if dir and file have double slash",
-      inputs: ["dir//", ["//f1"]],
-      expected: ["dir/f1"],
-    },
-    {
-      label: "if sub dir has double slash",
-      inputs: ["dir1/", ["//dir2//"]],
-      expected: ["dir1/dir2/"],
-    },
-    {
-      label: "if dir has root double shash",
-      inputs: ["//dir1/", ["/dir2/"]],
-      expected: ["/dir1/dir2/"],
-    },
-  ];
-  for (const test of tests) {
-    await t.step(test.label, () => {
-      const got = prefixDir(...test.inputs);
-      assertEquals(got, test.expected);
-    });
-  }
-});
+tableDrivenTestThrow("prefixDir - arugment prefix for throwing error", [
+  {
+    label: "if it has no suffix with slash",
+    inputs: ["base", ["f1"]],
+  },
+  {
+    label: "if it is empty",
+    inputs: ["", ["f1"]],
+  },
+]);
+
+tableDrivenTest("prefixDir - combine continuous slash into one", [
+  {
+    label: "if dir and file have double slash",
+    inputs: ["dir//", ["//f1"]],
+    expected: ["dir/f1"],
+  },
+  {
+    label: "if sub dir has double slash",
+    inputs: ["dir1/", ["//dir2//"]],
+    expected: ["dir1/dir2/"],
+  },
+  {
+    label: "if dir has root double shash",
+    inputs: ["//dir1/", ["/dir2/"]],
+    expected: ["/dir1/dir2/"],
+  },
+]);
